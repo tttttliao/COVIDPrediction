671ecc2c37b1beeeed747788306ed8e6e14d5321
tttttliao <35547532+tttttliao@users.noreply.github.com>
2020-11-11 22:44:17 +0800

    Add State name and data processing
    load data and change state name to state indices. Change date to difference from baseline date.

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import pandas
+from datetime import datetime
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%m-%d-%Y")
+    d2 = datetime.strptime(d2, "%m-%d-%Y")
+    return abs((d2 - d1).days)
+
+base_line_date = "04-12-2020"
+
 def main():
-    pass
+    data = pandas.read_csv("data/train.csv")
+
+    states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+    state_to_idx_dict = {}
+    for idx, state in enumerate(states):
+      state_to_idx_dict[state] = idx
+
+    # Drop ID column as it's not helpful in our prediction.
+    first_column = data.columns[0]
+    data = data.drop([first_column], axis=1)
+    data = data.values
+
+    # Change state name to corresponding state indices.
+    # Change date to difference from baseline date.
+    for i in range(data.shape[0]):
+        data[i][0] = state_to_idx_dict[data[i][0]]
+        data[i][1] = days_between(base_line_date, data[i][1])
+
+    print(data[0])
 
 if __name__ == '__main__':
     main()
