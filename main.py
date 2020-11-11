350de9cfbb8033c9ac2ebc7e296e910644ecdd94
tttttliao <35547532+tttttliao@users.noreply.github.com>
2020-11-11 22:53:01 +0800

    Modulize Pre-processing
    Separate pre-processing into function and separate label from attributes.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas
+import numpy as np
 from datetime import datetime
 
 def days_between(d1, d2):
@@ -7,16 +8,13 @@ def days_between(d1, d2):
     return abs((d2 - d1).days)
 
 base_line_date = "04-12-2020"
+states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+state_to_idx_dict = {}
+for idx, state in enumerate(states):
+  state_to_idx_dict[state] = idx
 
-def main():
-    data = pandas.read_csv("data/train.csv")
-
-    states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
-    state_to_idx_dict = {}
-    for idx, state in enumerate(states):
-      state_to_idx_dict[state] = idx
-
-    # Drop ID column as it's not helpful in our prediction.
+def pre_process(data):
+     # Drop ID column as it's not helpful in our prediction.
     first_column = data.columns[0]
     data = data.drop([first_column], axis=1)
     data = data.values
@@ -26,8 +24,16 @@ def main():
     for i in range(data.shape[0]):
         data[i][0] = state_to_idx_dict[data[i][0]]
         data[i][1] = days_between(base_line_date, data[i][1])
+    confirmed = data[:,2]
+    death = data[:,3]
+    data = np.delete(data, 2, 1)
+    data = np.delete(data, 2, 1)
 
-    print(data[0])
+    return data, confirmed, death
+
+def main():
+    data = pandas.read_csv("data/train.csv")
+    data, confirmed, death = pre_process(data)
 
 if __name__ == '__main__':
     main()
